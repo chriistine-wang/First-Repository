167db72739c77f9287929c76957ded803f6dd80b
chriistine-wang <qwang2@stu.naperville203.org>
2022-08-25 14:20:32 -0500

    new stuff

---
+++ Christine Wang/milage.java
@@ -0,0 +1,31 @@
+
+/**
+ * Christine Wang
+ * 8/23/22
+ * Milage pair assignment
+ * -------------------------------
+ * Prints which fraction of your car's use is for commuting to work & personal use.
+ * Reads
+ * -------------------------------
+ * Who helped me:
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class milage
+{
+    /**
+     * main method
+     */
+    public static void main(String[] args)
+    {
+    }
+}
+// 
+        // Calculate: workMiles = work days * one-way-distance * 2
+        // Calculate: milesTravelled = ending miles - beginning miles
+        // Calculate: workFraction = workMiles / milesTravelled
+        // Calculate: personalFraction = 1 - workFraction
