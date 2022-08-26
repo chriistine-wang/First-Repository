1c1d6a58bd28a24da694af6e06b6544f5200d975
chriistine-wang <qwang2@stu.naperville203.org>
2022-08-26 14:15:18 -0500

    sstuff

--- Christine Wang/milage.java
+++ Christine Wang/milage.java
@@ -1,13 +1,12 @@
 
 /**
- * Christine Wang
+ * Christine Wang, Mae Binkowski
  * 8/23/22
  * Milage pair assignment
  * -------------------------------
- * Prints which fraction of your car's use is for commuting to work & personal use.
- * Reads
+ * Prints which fraction of your car's use is for commuting to work & personal use after reading input from user
  * -------------------------------
- * Who helped me:
+ * Who helped me: Mr. Hayes, Mae
  */
 
 import java.util.Scanner;
@@ -22,6 +21,59 @@ public class milage
      */
     public static void main(String[] args)
     {
+        Scanner keyboard = new Scanner(System.in);
+                
+        double milesTravelled;
+        double ending_miles;
+        double beginning_miles;
+        double workFraction;
+        double personalFraction;
+        
+        double totalDays;
+        double onewayDistance = 5;
+        
+        // input from user: beginning and ending milage, number of work days
+
+        System.out.println ("This will calculate what fraction of your car's use is for commuting to work & personal use.");
+
+        System.out.println ("Please enter the number of days you spent using your car: ");
+        totalDays = keyboard.nextDouble();
+        
+        System.out.println ("Please enter the number of in those " + totalDays + " days you spent commuting for work.");
+        double workDays = keyboard.nextDouble();
+        
+        double workMiles = workDays * onewayDistance * 2;
+        
+        System.out.println ("Please enter the amount of miles you started with: ");
+        beginning_miles = keyboard.nextDouble();
+        
+        System.out.println ("Please enter the amount of miles you ended with: ");
+        ending_miles = keyboard.nextDouble();
+        
+        // Computes the percentage
+        
+        milesTravelled = ending_miles - beginning_miles;
+        
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        workFraction = workMiles / milesTravelled;
+        
+        personalFraction = 1 - workFraction;
+        
+        System.out.printf("Work Fraction: %-5s \t Personal Fraction:%-5s \n", (percent.format(workFraction)), (percent.format(personalFraction)));
+        
+        
+        
+
+        
+        
+
+        
+        
+    
+        
+        
+        
+        
     }
 }
 // 
